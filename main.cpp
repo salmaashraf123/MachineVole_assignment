55245593f3d10e196ae29e71a3c1ceaeaf51d5a1
salmaashraf123 <162500115+salmaashraf123@users.noreply.github.com>
2024-11-07 14:00:13 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "A1_Task4_S27_20231174_S8_20231073_S6_20231180.h"
+using namespace std;
+
+int main() {
+    mainUI mainUI;
+    cpu processor;
+
+    mainUI.run(processor);
+
+    return 0;
+}
